83603e24d74254d79211e425b9c09ac07833e9f3
hugorince <hugo.rince@gmail.com>
2023-06-12 09:41:31 +0200

    highscorer of each level display

--- pages/components/highscores/HighscoreQuizDisplay.tsx
+++ pages/components/highscores/HighscoreQuizDisplay.tsx
@@ -7,19 +7,22 @@ const HighscoreQuizDisplay = () => {
   return (
     <>
       {difficulty === "easy" ? (
-        <div>
-          Highscore in {difficulty} is {highscores.easy.score} by{" "}
-          {highscores.easy.name}
+        <div className="flex flex-col items-center">
+          <p className="absolute">{highscores.easy.name}</p>
+          <span className="relative bottom-5">👑</span>
+          <p>{highscores.easy.score}</p>
         </div>
       ) : difficulty === "medium" ? (
-        <div>
-          Highscore in {difficulty} is {highscores.medium.score} by{" "}
-          {highscores.medium.name}
+        <div className="flex flex-col items-center">
+          <p className="absolute">{highscores.medium.name}</p>
+          <span className="relative bottom-5">👑</span>
+          <p>{highscores.medium.score}</p>
         </div>
       ) : (
-        <div>
-          Highscore in {difficulty} is {highscores.hard.score} by{" "}
-          {highscores.hard.name}
+        <div className="flex flex-col items-center">
+          <p className="absolute">{highscores.hard.name}</p>
+          <span className="relative bottom-5">👑</span>
+          <p>{highscores.hard.score}</p>
         </div>
       )}
     </>
